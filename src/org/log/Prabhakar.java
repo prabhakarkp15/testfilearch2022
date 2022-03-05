5da8b7f838869ee671523f099725e53e21127129
prabhakarkp15 <prabhakar1997615.kp@gmail.com>
2022-03-05 17:42:23 +0530

    hello

---
+++ src/org/log/Prabhakar.java
@@ -0,0 +1,41 @@
+package org.log;
+
+import java.util.Scanner;
+
+public class Prabhakar {
+//	public static void main(String[] args) {
+//		Scanner h = new Scanner(System.in);
+//		System.out.println("enter the year");
+//		int year = h.nextInt();
+//
+//		if (year % 4 == 0 ){
+//			
+//			if (year % 100 == 0 ) {
+//				if (year % 400 == 0 ) {
+//					System.out.println("is an leep year");
+//				}
+//				else {
+//					System.out.println("is not an leeppnumber");
+//				}
+//			}
+//			else {
+//				System.out.println("not an leepnumber");
+//			}
+//			
+//			
+//		
+//	
+//		}
+//		else {
+//			System.out.println("not an leepnumber");
+//		}
+//		
+//	}
+	private int i=4;
+	private void pi() {
+		System.out.println(i);
+
+	}
+	
+}
+	
